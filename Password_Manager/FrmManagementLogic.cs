bda87e7f6d2424812b7247f8d4dfe5504cc1cc6b
CaroKann <th1475369@gmail.com>
2020-11-06 16:04:31 +0700

    show data

--- Password_Manager/FrmManagementLogic.cs
+++ Password_Manager/FrmManagementLogic.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Data_Transfer_Object;
 using Data_Access_Object;
 using System.Security.Cryptography;
+using System.Data;
+using Utils;
 
 namespace Password_Manager
 {
@@ -27,42 +29,51 @@ namespace Password_Manager
             }
         }
 
-        private FrmManagementLogic() { }
-
-        private string ComputeSha256Hash(string rawData)
+        public UserDAO UserDAO
         {
-            // Create a SHA256   
-            using (SHA256 sha256Hash = SHA256.Create())
+            get => default;
+            set
             {
-                // ComputeHash - returns byte array  
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
-
-                // Convert byte array to a string   
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
             }
         }
 
+        private FrmManagementLogic() { }
+
         public UserDTO CheckLogin(string Username, string Password)
         {
-            UserDTO user = UserDAO.Instance.CheckLogin(Username, ComputeSha256Hash(Password));
+            UserDTO user = UserDAO.Instance.CheckLogin(Username, StringCipher.ComputeSha256Hash(Password));
             user.Password = Password;
             return user;
         }
 
         public void CreateUser(string Username, string Password, string Phone)
         {
-            Password = ComputeSha256Hash(Password);
+            Password = StringCipher.ComputeSha256Hash(Password);
             UserDAO.Instance.CreateUser(Username, Password, Phone);
         }
 
         public void AddAccount(AccountDTO account)
         {
             AccountDAO.Instance.AddAccount(account);
+        }
+
+        public DataTable GetAccounts(UserDTO user)
+        {
+            DataTable accounts =  AccountDAO.Instance.GetAccounts(user.Username);
+            // On all tables' rows
+            foreach (DataRow dtRow in accounts.Rows)
+            {
+                // On all tables' columns
+                foreach (DataColumn dc in accounts.Columns)
+                {
+                    if (dc.ColumnName != "id")
+                    {
+                        dtRow[dc] = StringCipher.Decrypt(dtRow[dc].ToString(), user.Password);
+                    }
+                }
+            }
+            Console.WriteLine(accounts);
+            return accounts;
         }
     }
 }
